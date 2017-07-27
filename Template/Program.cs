84fb9ff28d9a92cc1ad62fe72a1f3fb15efe1215
Dev4Life <jl364@hotmail.co.uk>
2017-07-27 14:04:41 +0100

    basic console application template

---
+++ Template/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SPSite sc = new SPSite("http://dev-jl364/eval/vs2013"))
+            {
+                foreach (SPWeb site in sc.AllWebs)
+                {
+                    Console.WriteLine(site.Title);
+                }
+            }
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+
+    }
+}
